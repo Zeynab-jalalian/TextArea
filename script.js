908b0c4e4ebeea9902a88a002039b4eaf90ae3ac
zeynab jalalian <zjalalianweb@gmail.com>
2024-12-29 20:41:06 +0330

    js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let textarea_resize=document.querySelector('textarea');
-textarea_resize.addEventListener('keyup' , function(e)){
-
+let textarea_resize = document.querySelector('textarea');
+textarea_resize.addEventListener('keyup', function (e) {
+    textarea_resize.style.height = '53px'
+    let text_area_height = e.target.scrollHeight;
+    textarea_resize.style.height = text_area_height + "p";
 });
